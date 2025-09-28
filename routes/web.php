82a8cacf32bf23b5ab989c9b3389b652ab82cb40
Herusweb <herusweb@gmail.com>
2025-09-28 19:20:10 +0200

    Setup Vue 3, TailwindCSS, Vite and Redis integration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+// routes/web.php
+use Illuminate\Support\Facades\Cache;
+
+Route::get('/redis', function () {
+    Cache::store('redis')->put('test-key', 'Hello Redis', 600); // сохраняем 10 минут
+    return Cache::store('redis')->get('test-key'); // получаем
 });
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true, // иногда нужно на WSL/Windows, чтобы HMR работал
+            interval: 100,
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        vue(),
         tailwindcss(),
     ],
 });
